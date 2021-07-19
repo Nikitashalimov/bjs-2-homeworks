7ec19003028b0752dd1ad235be1363cf9f461e4b
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2021-07-19 20:10:54 +0300

    Add files via upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,10 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
+    result = true;
+  } else {
+    result = false;
+  }
   return result; // boolean
 }
 
