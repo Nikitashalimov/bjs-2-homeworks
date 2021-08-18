cdd2b9d6e821e9e675ee1c18f8c7ea5da835762d
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2021-08-18 22:57:01 +0300

    Add files via upload

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,51 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash === hash);
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args);
+      cache.push({ hash: hash, value: result });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+
+function debounceDecoratorNew(f, ms) {
+  let timeout = null;
+  return function (...args) {
+    if (timeout == null) {
+      setTimeout(() => {
+        timeout = null;
+        f(...args);
+      }, ms);
+    }
+  };
+};
+
+
 
 function debounceDecorator2(func) {
-  // Ваш код
-}
+  let timeout = null;
+  wrapper.count = 0;
+  return function wrapper(...args) {
+    wrapper.count++;
+    if (timeout == null) {
+      setTimeout(() => {
+        timeout = null;
+        func(...args);
+      });
+      console.log(wrapper.count);
+    }
+  };
+};
